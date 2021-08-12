a83f7bccc68055e8b77148329c9cf4d6056127de
LucasKohanevicRadice <77052588+LucasKohanevicRadice@users.noreply.github.com>
2021-08-12 17:22:54 +0300

    updated

---
+++ Top_picks/osa07-16_spellchecker_versio2/src/spellchecker.py
@@ -0,0 +1,43 @@
+# tee ratkaisu tänne
+
+from difflib import get_close_matches
+import string
+
+def spellchecker_v2():
+
+    for i in range(1,3):
+        print()
+    syote = input("Write a text in english: ")
+    syote = syote.lower()
+
+    korjattusyote = ""
+    sanalista = []
+
+    with open("wordlist.txt") as tiedosto:
+
+        for rivi in tiedosto:
+            rivi = rivi.strip()
+            rivi = rivi.lower()
+            sanalista.append(rivi)
+    
+    sanat = syote.split(" ")
+
+    korjauskehotus = f"korjausehdotukset: \n"
+    
+
+    for sana in sanat:
+        if sana not in sanalista:
+            vääräsana = sana
+            korjattusyote += f"*{vääräsana}*" + " "
+            korjauskehotus += f"{vääräsana}: {get_close_matches(vääräsana,sanalista, n=3)}\n"
+
+        else:
+            korjattusyote += sana + " "
+    korjattusyote = korjattusyote.strip()
+
+    print()
+    print(korjattusyote)
+    print()
+    print(korjauskehotus)
+
+spellchecker_v2()
